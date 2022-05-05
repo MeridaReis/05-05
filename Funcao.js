9afbbd8ae0c95dcd26c86fbab83d8e07b5b08902
Merida <102757659+MeridaReis@users.noreply.github.com>
2022-05-05 15:48:40 -0300

    Add files via upload

---
+++ Funcao.js
@@ -0,0 +1,29 @@
+function nome (n) {
+    console.log(n)
+    return
+}
+
+nome('Marina')
+
+function soma (a,b){
+    console.log(a+b)
+    return
+}
+
+soma(5,7)
+
+function mult (e,f){
+    let mult= e*f;
+        console.log(mult/4)
+        return
+}
+
+mult (5,6)
+
+function conta(r,s,t,u){
+    let res= r+s+t+u
+    console.log
+}
+
+conta (5,6,7,8)
+
